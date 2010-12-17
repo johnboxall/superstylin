4b9673fd7a203bd14b495dfd1f2141acaecdbad3
John Boxall <john@mobify.me>
2010-12-17 13:51:07 -0800

    Add hooks to change how data is processed into CSS (yeah less.js!)

--- superstylin.js
+++ superstylin.js
@@ -1,312 +1,258 @@
-/**
-  * Superstylin - MIT Licensed, John Boxall
-  * http://github.com/johnboxall/superstylin
-  *
-  * Edit stylesheets from the comfort of your browser.
-  *
-  **/
-function superstylin() {    
-    var ss = window.ss || {};
+this.superstylin = (function (window, document, undefined) {    
+    var popupLoaded = false,
+        // True if there are unsaved changes to Styles.
+        dirty = false,
+        // Used to check whether URLs are remote.
+        rurl = /^(\w+:)?\/\/([^\/?#]+)(.*)/,
+        popup,
+        $;
+        
+    function stylin() {
+        if (typeof jQuery === 'undefined') {
+            var script = document.createElement('script'),
+                scriptLoaded = false;
+            
+            script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js';
+            script.onload = function() {
+                if (!scriptLoaded) {
+                    scriptLoaded = true;
+                    $ = jQuery.noConflict();
+                    init();
+                }
+            };
+            document.head.appendChild(script);
+        } else {
+            $ = jQuery;
+            init();
+        }
+    };
     
-    // If already open, noop.
-    if (ss.pop && !ss.pop.closed){
-        return;
-    }
+    function join(o) {
+        var l = [], p;
+        for (p in o) l.push(p + '=' + o[p]);
+        return l.join(',');
+    };
     
-    // Stash for content of stylesheets.
-    var cache = ss.cache = ss.hasOwnProperty("cache") && ss.cache || {};
-    // Warn if there are unsaved changes.
-    var dirty = false;
-    // RegExp used for detecting external urls.
-    var rurl = /^(\w+:)?\/\/([^\/?#]+)/;
-    var undefined;
-    var $;
+    function Style(styleSheet) {
+        var href = styleSheet.href;
+        if (href) {
+            var match = rurl.exec(styleSheet.href || '');
+            
+            this.hostname = match[2];
+            this.url = match[3];
+            this.styleSheet = styleSheet;
+        }
+    };
+    
+    stylin.Style = Style;
+    Style.styles = [];
+    
+    Style.prototype.shouldLoad = function() {
+        return this.hostname == location.hostname;
+    };
+    
+    Style.prototype.fetch = function(onComplete) {
+        $.ajax({
+            url: this.url,
+            complete: onComplete,
+            cache: false
+        });
+    };
+    
+    Style.prototype.load = function(onLoaded) {
+        var self = this;
+        self.fetch(function(xhr, statusText) {
+            if (statusText == 'success') {
+                self.el = document.createElement('style');
+                self.el.type = 'text/css';
+                self.el.appendChild(document.createTextNode(''));
+                self.styleSheet.ownerNode.parentNode.replaceChild(self.el, self.styleSheet.ownerNode);
+                self._data = xhr.responseText;
+                self.update(null, 0);
+                
+                Style.styles.push(self);
+                
+                onLoaded();
+            } else {
+                alert('Failed loading: ' + self.href);
+            }
+        });
+    };
+    
+    Style.prototype.update = function(data, timeout) {
+        var self = this,
+            dataIsNull = data === null;
+        
+        if (dataIsNull) {
+            data = self._data;
+        }
+        
+        // Update when we have new data.
+        if (data != self.data) {
+            if (dataIsNull) {
+                dirty = true
+            }
+            
+            self.data = data;
+            
+            // Stagger renders to stay responsive.
+            clearTimeout(self.updateTimeout);
+            
+            self.updateTimeout = setTimeout(function() {
+                self.beforeRender(data, function() {
+                    self.render.apply(self, Array.prototype.slice.call(arguments));
+                });
+            }, timeout);
+        }
+    };
+    
+    // Hook to alter how data is rendered to CSS.
+    Style.prototype.beforeRender = function(data, onComplete) {
+        onComplete(data);
+    };
+    
+    Style.prototype.render = function(css) {
+        this.el.replaceChild(document.createTextNode(css), this.el.firstChild);
+    };
+    
+    Style.prototype.save = function(onComplete) {
+        // You get to implement this.
+        alert('Not Implemented!');
+    };
 
-    function join(obj) {
-        var attrs = [];
-        
-        for (var key in obj) {
-            attrs.push(key + "=" + obj[key]);
+    function init() {
+        function load() {
+            buildPopup();
+            bindEvents();
         }
-        return attrs.join(",");
-    }
-    
-    function getJS(src, win) {
-        var script = document.createElement('script');
-        var head = document.getElementsByTagName('head')[0];
-        var done = false;
         
-        script.src = src;
-        script.onload = function() {
-            if (!done){
-                done = true;
-                if (win){
-                    win();
-                }
-            }
-        };
-        head.appendChild(script);
-    }
+        if (!popupLoaded) {
+            popupLoaded = true;
+                    
+            // Fetch the stylesheets and replace them with editable style elements.
+            // Then build the interface and bind the events!
+            var toLoad = 0;
+            
+            $.each(document.styleSheets, function() {
+                var style = new Style(this);
+                if (style.shouldLoad()) {
+                    toLoad++;
     
-    function getCSS(url, win, fail){
-        var m = rurl.exec(url);
-        // Try changing to current protocol when host is the same.
-        var remote = false;
-        if (m[1] && m[1] !== location.protocol) {
-            if (m[2] !== location.host) {
-                remote = true;
-            } else {
-                url = location.protocol + url.slice(url.indexOf(":") + 1)
-            }
-        }
+                    // TODO: What if style fails to load?
+                    style.load(function() {
+                        toLoad--;
                         
-        // TODO: YQL Sucks. Just need a straight proxy.
-        // Use YQL for cross-domain requests.
-        if (remote) {
-            var q = 'select * from html where url="' + url + '"';
-            var src = 'http://query.yahooapis.com/v1/public/yql?q=' + q + '&callback=_yql';
-            
-            // Massage response into expected format.
-            window._yql = function(r) {
-                if (!r.results.length) {
-                    alert(r.query.diagnostics.forbidden);
-                } else {
-                    // Response wrapped in <body><p>(.*?)</p></body>
-                    var css = r.results[0].substr(14, r.results[0].length - 28);
-                    win(css);
-                }                   
-            };
-            getJS(src);
-        } else {
-            $.ajax({
-                url: url,
-                cache: false,
-                complete: win
-            });
-        }
-    }
-    
-    // Load a stylesheet and make it editable.
-    function open(link) {
-        var self = link;
-        var url = link.innerHTML;
-        var $self = $(link);
-        var $textarea = $self.next().show().children("textarea");
-        var styleSheet;
-        
-        // Cache stylesheets so we only request them once.
-        if (cache.hasOwnProperty(url)) {
-            return false;
-        }
-        cache[url] = true;
-        
-        // TODO: Handle inline stylesheets.
-        // Find find the stylesheet node we wanted to edit.
-        for (var i = 0; i < document.styleSheets.length; i++) {
-            var styleSheet = document.styleSheets[i];
-            if (styleSheet.href && styleSheet.href == url) {
-                break;
-            }
-        }
-        
-        // TODO: Problem with order of operations causes "undefined to flash.
-        
-        // Get the CSS then bind keyup/keydown to update it.
-        getCSS(url, function(xhr) {
-            var _update;
-            
-            // IE is happy to change the styleSheet element.
-            if (document.styleSheets[i].cssText) {
-                _update = function(s) {
-                    document.styleSheets[i].cssText = s;
-                }
-            // For others, remove the styleSheet and edit a style element.
-            } else {
-                var css = document.createElement("style");
-                css.type = "text/css";
-                styleSheet.ownerNode.parentNode.replaceChild(css, styleSheet.ownerNode);
-                
-                _update = function(s) {
-                    var node = document.createTextNode(s)
-                    if (css.childNodes.length > 0) {
-                        if (css.firstChild.nodeValue !== node.nodeValue) {
-                            css.replaceChild(node, css.firstChild);
+                        if (toLoad == 0) {
+                            load();
                         }
-                    } else {
-                        css.appendChild(node);
-                    }
+                    });
                 }
-            }
-            
-            function update(ignoreDirty) {
-                var val = this.value || "";
-                if (val != cache[url]) {
-                    if (ignoreDirty !== true) {
-                        dirty = true;
-                    }
-                    _update(val);
-                    cache[url] = val;
-                }
-            }
-            
-            $textarea
-                .keyup(update)
-                .keydown(update)
-                .val(xhr.responseText);
-                
-            update.call($textarea[0], true);
-            
-        });      
-          
+            });
+        } else {
+            load();
+        }
     }
     
-    // Build the interface in a popup window.
-    function init() {    
-        var name = ss.popUpName || "superstylin";
-        var opts = ss.popUpOpts || {
-                width: screen.width * 0.33, 
+    function buildPopup() {
+        var opts = {
+                width: parseInt(screen.width * 0.33),
                 height: screen.height,
-                location: false
+                screenY: 0,
             };
-        opts.screenX = screen.width - opts.width;
         
-        var pop = window.open("", name, join(opts));
-        // TODO: IE, if pop didn't open, pop.document might not be there?
+        opts.screenX = screen.width - opts.width;        
+        popup = window.open('', 'a', join(opts));
         
-        // If we were open before, then load from cache.
-        if (ss.$docEl && ss.$docEl.length) {
-            var docEl = pop.document.documentElement;
-            docEl.parentNode.replaceChild(ss.$docEl[0], docEl);
-        } else {
-            // TODO: Make this pretty.
-            var style = ss.style || "ul{margin:0;padding:0;list-style:none;}li{margin:1em 0.1em;padding:0.5em;border:1px solid #ddd;background:#f8f8ff;}textarea{clear:both;width:100%;height:90%;font-family: Monaco,'Courier New';font-size:10px;}a{color:#4183C4;}div{position:relative;top:-20px;text-align:right;}a.close{color:#777;}.winning{background:black;}.winning.win{background:yellow;}.fail{background:red;}";
-            var html = ['<script type="text/javascript">var open=1;</script><style>'+ style + '</style><ul>'];
-            for (var i = 0; i < document.styleSheets.length; i++){
-                var href = document.styleSheets[i].href;     
-                if (href && (ss.ignore == undefined || ss.ignore(href))) {
-                    html.push('<li> \
-    <a id="ss'+ i +'" class="open" href="#">' + href +'</a> \
-    <div style="display:none;"> \
-        <a class="close" href="#">Close</a> \
-        <input type="submit" value="Save" /> \
-        <textarea name="' + (ss.name || href) + '"></textarea> \
-    </div> \
-</li>');
-                }
-            }
-            html.push("</ul>");            
-            pop.document.write(html.join(""));
-        }
+        var html = [
+                '<script type="text/javascript">var open = 1;</script>',
+                '<style type="text/css">',
+                    '.styleSheet {}',
+                    '.styleSheet div { float: right; }',
+                    '.styleSheet.closed textarea { display: none; }',
+                    'textarea { width: 100%; height: 90%; font-family: Monaco, "Courier New"; font-size: 10px; padding: 10px; }',
+                    '.saving { background: red; }',
+                '</style>'
+            ];
         
-        pop.document.close();
-        pop.focus();
+        $.each(Style.styles, function(id, style) {
+            html.push(
+                '<div class="styleSheet closed" data-id="' + id + '">',
+                    style.url,
+                    '<div>',
+                        '<button>Toggle</button>',
+                        '<input type="submit" value="Save" />',
+                    '</div>',
+                '<textarea>' + style._data + '</textarea>',
+                '</div>'
+            );
+        });
         
-        // TODO: This throws in IE even if things worked.
+        popup.document.write(html.join(''));
+        popup.document.close();
+        popup.focus();
+        
         // http://stackoverflow.com/questions/668286/detect-blocked-popup-in-chrome
         setTimeout(function() {
-            if (!pop || pop.closed || typeof pop.closed == "undefined" || !pop.open || !pop.outerHeight){
-                alert("Check your popup blocker settings.");
+            if (!popup || popup.closed || typeof popup.closed === 'undefined' || !popup.open || !popup.outerHeight) {
+                alert('Check your popup blocker settings.');
             }
         }, 1);
-        
-        // Expose pop so we can test if it's open.
-        ss.pop = pop;
-        
-        // Populate open textareas from cache. Helps when we're reopening pop.
-        $("textarea", pop.document).each(function() {
-            var url = $(this).parent().prev("a").html();
-            this.value = cache[url];
-        })
-        
-        // Serialize pop state on close so we can resume where we left off.
-        pop.onbeforeunload = function() {
-            ss.$docEl = $(ss.pop.document.documentElement).clone(true);
-        }
-        
-        // If we have been open before, no need to rebind handlers.
-        if (ss.$docEl && ss.$docEl.length) {
-            return;
-        }
-        
-        var $openers = $("a.open", pop.document);
-        
-        // If there is only one stylesheet, automatically open it.
-        if ($openers.length == 1) {
-            open($openers.get(0));
-        }
-        
-        $openers.click(function() {
-            open(this);
-            return false;
+    }
+    
+    function bindEvents() {      
+        $(popup.document).delegate('textarea', 'keyup keydown', function() {
+            var id = $(this).parents('.styleSheet').data('id');
+            Style.styles[id].update(this.value || '', 250);
         });
         
-        $("a.close", pop.document).click(function() {
-            $(this).parent().hide();
-            return false;
+        $(popup.document).delegate('button', 'click', function() {
+            $(this).parents('.styleSheet').toggleClass('closed');
         });
         
-        // Saving stylesheets on click.
-        $save = $("input", pop.document);
-        $save.click(save);
+        // Save Style.
+        $(popup.document).delegate('input[type="submit"]', 'click', function() {
+            var saveEl = this,
+                id = $(this).parents('.styleSheet').data('id');
 
-        // TODO: Doesn't make sense if you have multiple sheets.
-        // Saving stylesheets on ctrl+s.
-        $(pop.document).keydown(function(e) {
-            var character = String.fromCharCode(e.which).toLowerCase();
-            if (character == "s" && e.ctrlKey) {
-                save.call($save[0]);
+            Style.styles[id].save(function(xhr, textStatus) {
+                if (textStatus == 'success') {
+                    dirty = false;
+                } else {
+                    alert('Error saving CSS: ' + xhr.statusText);
+                }
+                saveEl.className = '';
+            });
+            
+            saveEl.className = 'saving';            
+        });
+        
+        // Save Styles with CTRL+S.
+        $(popup.document).keydown(function(e) {        
+            var char = String.fromCharCode(e.which).toLowerCase();
+            if (char == 's' && e.ctrlKey) {
+                $('input[type="submit"]', popup.document).click();
                 e.preventDefault();
                 return false;
             }
         });
         
-        // Close pop when you leave the page.
-        // TODO: Option to save automatically on close?
-        // TODO: Only warn about dirty once?
+        // If there is one Style, open it.
+        if (Style.styles.length == 1) {
+            $('button', popup.document).click();
+        }
+        
         window.onbeforeunload = function() {
             if (dirty) {
                 return 'You have unsaved CSS changes.';
-            } else {
-                ss.pop.close();
             }
-        }
-        
-        function save() {
-            var $self = $(this);
-            var textarea = $self.next()[0];
-            var data = {};
-            data[ss.name || textarea.name] = textarea.value;
+        };
             
-            $.ajax({
-                type: "POST", 
-                url: ss.saveTo, 
-                data: data,
-                success: function() {
-                    dirty = false;
-                    $self.addClass("win");
-                    setTimeout(function(){
-                        $self.removeClass("winning").removeClass("win");
-                    }, 1000);
-                }, error: function(x) {            
-                    $self.addClass("fail");
-                    alert("Couldn't save your CSS. " + x.statusText);
-                }
-            });
-            $self.addClass("winning");            
-        }        
+        window.onunload = function() {
+            popup.close();
+        };
         
-    } // init()
-    
-    if (typeof jQuery == 'undefined') {
-        getJS('http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js', function() {
-            $ = jQuery.noConflict();
-            init();
-        });
-    } else {
-        $ = jQuery;
-        init();
     }
     
-    return false;
-    
-} // superstylin()
+    return stylin;
+
+})(window, document);
